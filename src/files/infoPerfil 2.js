a67753875a26099cf9bf6e4b73c1fbc05b0595e1
Andrea Núñez <a01640839@itesm.mx>
2023-06-14 13:27:06 -0600

    retroUsoDeMarca

--- src/files/infoPerfil 2.js
+++ src/files/infoPerfil 2.js
@@ -1,7 +1,7 @@
 const perfil = [
     {
         id: 1,
-        nombre: 'Juan',
+        name: 'Hugo',
         apellido: 'Perez',
         email: 'j.perez@hotmail.com',
         img: "https://marketplace.canva.com/EAFEits4-uw/1/0/1600w/canva-boy-cartoon-gamer-animated-twitch-profile-photo-oEqs2yqaL8s.jpg",
